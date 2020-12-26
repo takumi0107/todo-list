9ee35677ef9f4233ab46c1390573e16c0b5d02a1
Your Name <you@example.com>
2020-12-26 18:07:35 +0900

    commit message

---
+++ Procfile
@@ -0,0 +1,1 @@
+web:gunicorn editdojo.wsgi
---
+++ editdojo/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for editdojo project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'editdojo.settings')
+
+application = get_asgi_application()
